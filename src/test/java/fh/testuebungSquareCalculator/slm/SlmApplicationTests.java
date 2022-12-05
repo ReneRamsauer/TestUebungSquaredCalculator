dfa51abd75b523fd009955ca7efd568dd8428b54
ic18b066 <ic18b066@technikum-wien.at>
2022-12-05 22:52:09 +0100

    Initial commit

---
+++ src/test/java/fh/testuebungSquareCalculator/slm/SlmApplicationTests.java
@@ -0,0 +1,13 @@
+package fh.testuebungSquareCalculator.slm;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SlmApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
